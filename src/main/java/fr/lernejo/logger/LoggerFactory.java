36a61ca7f4af00e74ff83054c4b70eac018a746e
Lucas Mari <mari@et.esiea.fr>
2021-12-01 10:26:24 +0100

    part 2

---
+++ src/main/java/fr/lernejo/logger/LoggerFactory.java
@@ -0,0 +1,12 @@
+package fr.lernejo.logger;
+
+public class LoggerFactory implements Logger{
+    @Override
+    public void log(String message) {
+        System.out.println(message);
+    }
+
+    public static Logger getLogger(String name){
+        return getLogger(name);
+    }
+}
